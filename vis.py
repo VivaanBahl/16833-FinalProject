6ce8cd70a24518690c01d82bb14d866b05fd001e
Vivaan Bahl <vivaanbahl@gmail.com>
2018-04-02 16:37:02 -0400

    added draft of visualization

--- vis.py
+++ vis.py
@@ -1,4 +1,6 @@
 import logging
+import matplotlib.pyplot as plt
+
 
 class Visualizer(object):
     def __init__(self, robots, motions):
@@ -14,12 +16,31 @@ class Visualizer(object):
         # Note that these are aliased to the real ones. Don't modify, just read.
         self.robots = robots
         self.motions = motions
+        self.figure_number = 1
 
+        self.viewport_x_min = -500
+        self.viewport_x_max = 500
+        self.viewport_y_min = -500
+        self.viewport_y_max = 500
 
     def update(self):
         """Perform visualization updates here."""
 
         self.logger.debug("Updating visualization.")
-        
-        for motion in self.motions:
-            print(motion.pos)
+
+        fig = plt.figure(self.figure_number)
+        fig.clf()
+        x = []
+        y = []
+
+        for i, motion in enumerate(self.motions):
+            self.logger.debug("Robot {} has pos {}, vel {}".format(i, motion.pos, motion.vel))
+
+            pos = motion.pos
+            x.append(pos[0])
+            y.append(pos[1])
+
+        plt.scatter(x, y)
+        plt.xlim([self.viewport_x_min, self.viewport_x_max])
+        plt.ylim([self.viewport_y_min, self.viewport_y_max])
+        plt.show()
