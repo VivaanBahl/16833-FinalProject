b9daed3d716a52e356da7f8875a2107ccb47f8e3
Vivaan Bahl <vivaanbahl@gmail.com>
2018-04-02 16:57:47 -0400

    Added in drawing goals for robots

--- vis.py
+++ vis.py
@@ -1,5 +1,5 @@
-import logging
 import matplotlib.pyplot as plt
+import logging
 
 
 class Visualizer(object):
@@ -18,10 +18,14 @@ class Visualizer(object):
         self.motions = motions
         self.figure_number = 1
 
-        self.viewport_x_min = -500
-        self.viewport_x_max = 500
-        self.viewport_y_min = -500
-        self.viewport_y_max = 500
+        self.viewport_x_min = -100
+        self.viewport_x_max = 100
+        self.viewport_y_min = -100
+        self.viewport_y_max = 100
+
+        fig = plt.figure(self.figure_number)
+        plt.ion()
+        plt.show()
 
     def update(self):
         """Perform visualization updates here."""
@@ -43,4 +47,14 @@ class Visualizer(object):
         plt.scatter(x, y)
         plt.xlim([self.viewport_x_min, self.viewport_x_max])
         plt.ylim([self.viewport_y_min, self.viewport_y_max])
+
+        x_goal = []
+        y_goal = []
+        for robot in self.robots:
+            goal = robot.goal
+            x_goal.append(goal[0])
+            y_goal.append(goal[1])
+
+        plt.scatter(x_goal, y_goal, c='g', marker='x')
+        plt.pause(0.05)
         plt.show()
