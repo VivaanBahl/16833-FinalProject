585892698d992a622128a2c64074176e28bfac2d
Vivaan Bahl <vivaanbahl@gmail.com>
2018-04-04 11:24:42 -0400

    incremental commit

--- vis.py
+++ vis.py
@@ -18,11 +18,6 @@ class Visualizer(object):
         self.motions = motions
         self.figure_number = 1
 
-        self.viewport_x_min = -100
-        self.viewport_x_max = 100
-        self.viewport_y_min = -100
-        self.viewport_y_max = 100
-
         fig = plt.figure(self.figure_number)
         plt.ion()
         plt.show()
@@ -32,29 +27,35 @@ class Visualizer(object):
 
         self.logger.debug("Updating visualization.")
 
+        # get the main update figure, clear it out for our update
         fig = plt.figure(self.figure_number)
         fig.clf()
-        x = []
-        y = []
+
+        # TODO convert to numpy arrays for scalability
+        x_robot = []
+        y_robot = []
+        x_goal = []
+        y_goal = []
 
         for i, motion in enumerate(self.motions):
             self.logger.debug("Robot {} has pos {}, vel {}".format(i, motion.pos, motion.vel))
 
             pos = motion.pos
-            x.append(pos[0])
-            y.append(pos[1])
+            x_robot.append(pos[0])
+            y_robot.append(pos[1])
 
-        plt.scatter(x, y)
-        plt.xlim([self.viewport_x_min, self.viewport_x_max])
-        plt.ylim([self.viewport_y_min, self.viewport_y_max])
-
-        x_goal = []
-        y_goal = []
-        for robot in self.robots:
+            robot = self.robots[i]
             goal = robot.goal
             x_goal.append(goal[0])
             y_goal.append(goal[1])
 
+        # draw robots
+        plt.scatter(x_robot, y_robot)
+
+        # draw goals as green x's
         plt.scatter(x_goal, y_goal, c='g', marker='x')
+
+        plt.xlim([self.viewport_x_min, self.viewport_x_max])
+        plt.ylim([self.viewport_y_min, self.viewport_y_max])
         plt.pause(0.05)
         plt.show()
