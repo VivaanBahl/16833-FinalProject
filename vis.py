4ebffbbd7ef5b3b146f3132277f0a02a94193f3f
Vivaan Bahl <vivaanbahl@gmail.com>
2018-04-07 16:03:47 -0400

    Added relative viewport scaling

--- vis.py
+++ vis.py
@@ -18,11 +18,6 @@ class Visualizer(object):
         self.motions = motions
         self.figure_number = 1
 
-        self.viewport_x_max = 100
-        self.viewport_x_min = -100
-        self.viewport_y_max = 100
-        self.viewport_y_min = -100
-
         fig = plt.figure(self.figure_number)
         plt.ion()
         plt.show()
@@ -42,6 +37,12 @@ class Visualizer(object):
         x_goal = []
         y_goal = []
 
+        # default viewport for the visualizer
+        x_min_coord = 0
+        x_max_coord = 0
+        y_min_coord = 0
+        y_max_coord = 0
+
         for i, motion in enumerate(self.motions):
             self.logger.debug("Robot {} has pos {}, vel {}".format(i, motion.pos, motion.vel))
 
@@ -54,13 +55,32 @@ class Visualizer(object):
             x_goal.append(goal[0])
             y_goal.append(goal[1])
 
+            # if a target is outside of the viewport, set viewport to include it
+            if pos[0] < x_min_coord or goal[0] < x_min_coord:
+                x_min_coord = min(pos[0], goal[0])
+            if pos[0] > x_max_coord or goal[0] > x_max_coord:
+                x_max_coord = max(pos[0], goal[0])
+            if pos[1] < y_min_coord or goal[1] < y_min_coord:
+                y_min_coord = min(pos[1], goal[1])
+            if pos[1] > y_max_coord or goal[1] > y_max_coord:
+                y_max_coord = max(pos[1], goal[1])
+
         # draw robots
         plt.scatter(x_robot, y_robot)
+
+        # draw "headings" aka the velocity vectors
+        ax = plt.axes()
 
         # draw goals as green x's
         plt.scatter(x_goal, y_goal, c='g', marker='x')
 
-        plt.xlim([self.viewport_x_min, self.viewport_x_max])
-        plt.ylim([self.viewport_y_min, self.viewport_y_max])
+        # calculate viewport bounds
+        x_scale = x_max_coord - x_min_coord
+        y_scale = y_max_coord - y_min_coord
+        x_margin = x_scale / 12
+        y_margin = y_scale / 12
+        plt.xlim([x_min_coord - x_margin, x_max_coord + x_margin])
+        plt.ylim([y_min_coord - y_margin, y_max_coord + y_margin])
+
         plt.pause(0.05)
         plt.show()
